1f3d78f742bc9341b0421f7cafcc6d1f19701083
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-10-04 16:43:27 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
 
+<<<<<<< HEAD
+=======
+  def show
+    require_login
+  end
+
+>>>>>>> f169b27f0ce1b244ba6f33786198671bf4240b77
   def new
   end
 
@@ -14,11 +21,23 @@ class SessionsController < ApplicationController
 
   def destroy
     if !current_user.nil?
+<<<<<<< HEAD
       session.delete :name
       redirect_to '/login'
+=======
+      current_user.destroy
+>>>>>>> f169b27f0ce1b244ba6f33786198671bf4240b77
     else
       redirect_to '/'
     end
   end
 
+<<<<<<< HEAD
+=======
+  private
+
+  def require_login
+    redirect_to '/login' unless session.include? :name
+  end
+>>>>>>> f169b27f0ce1b244ba6f33786198671bf4240b77
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
 
   get '/login', to: 'sessions#new'
 
@@ -7,4 +8,15 @@ Rails.application.routes.draw do
   post '/logout', to: 'sessions#destroy'
 
   get '/secrets', to: 'secrets#show'
+=======
+  get '/secrets', to: 'secrets#show'
+
+  get '/', to: 'sessions#show'
+
+  get '/login', to: 'sessions#new'
+
+  post '/login', to: 'sessions#create'
+
+  delete '/login', to: 'sessions#destroy'
+>>>>>>> f169b27f0ce1b244ba6f33786198671bf4240b77
 end
